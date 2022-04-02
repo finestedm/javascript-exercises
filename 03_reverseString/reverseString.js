774cc5fa79c7fe7e68855adef61467139ea36a07
pawel <finestedm@gmail.com>
2022-04-02 22:07:34 +0200

    3 complete, began working on 4th

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    reversedString = '';
+    len = ((string.length) + 1);
+    for (let i = 1; i < len; i++) {
+        reversedString += string.charAt(string.length - i);
+    }
+    return reversedString;
 
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...remover) {
+    let arrClean = arr;
+    let len = remover.length;
+    for (let i = 0; i < len; i++) {
+        var index = arrClean.indexOf(remover.[i]);
+        if (index !== -1) {
+            arrClean.splice(index, 1);
+        }
+    }
+    return arrClean
+}
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
