b0177e89b9128f892bbfd038c570a561ca246248
pawel <finestedm@gmail.com>
2022-04-03 22:13:36 +0200

    8 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (fahr) {
+  let celc = (fahr - 32) * 5 / 9;
+  celcRounded = Math.round(celc * 10) / 10;
+  return celcRounded;
 };
 
-const ctof = function() {
-
+const ctof = function (celc) {
+  let fahr = (celc * (9 / 5)) + 32;
+  fahrRounded = Math.round(fahr * 10) / 10;
+  return fahrRounded;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return (num1 + num2)
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return (num1 - num2)
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  var sum = 0;
+  for (i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  var sum = 1;
+  for (i = 0; i < array.length; i++) {
+    sum *= array[i];
+  };
+  return sum
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return (num1 ** num2)
 };
 
-const factorial = function() {
-	
+const factorial = function (num1) {
+  var fact = 1;
+  for (i = num1; i > 0; i--) {
+    fact *= i;
+  };
+  return fact
 };
 
 // Do not edit below this line
