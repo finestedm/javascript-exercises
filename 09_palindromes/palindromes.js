283fdcaa369f0a767886677a2f72544ba58117cc
pawel <finestedm@gmail.com>
2022-04-05 23:06:33 +0200

    9  complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    var wordClean = word.replace(/[!,. ]/g, '').toLowerCase()
+    var len = (wordClean.length);
+    var wordReversed = '';
+    for (i = 0; i <= len; i++) {
+        wordReversed += wordClean.charAt(len - i);
+    }
+    if (wordReversed == wordClean) {
+        return true
+    } else {
+        return false
+    }
 };
+
+//wordReversed.slice(i, (i + 1))
 
 // Do not edit below this line
 module.exports = palindromes;
