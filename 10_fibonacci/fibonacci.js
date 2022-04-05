4b3908f7f94c6c02dd6d9cabfa71e67e2606ffd4
pawel <finestedm@gmail.com>
2022-04-05 23:39:54 +0200

    10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+    if (index < 0) {
+        return 'OOPS';
+    } else {
+        index = parseInt(index);
+        sequence = [0];
+        for (i = 0; i < index; i++) {
+            if (sequence.length <= 1) {
+                sequence.push(1);
+            } else {
+                var another = (sequence[i] + sequence[i - 1]);
+                sequence.push(another);
+            }
+        }
+        return sequence[index];
+    }
 };
 
 // Do not edit below this line
